9438f01e21b9364b850931f5636214e7c32f6c86
Kodrillar <ipadeoladavid112@gmail.com>
2024-09-21 09:18:02 +0100

    Export DeviceSizes

--- lib/screendapt.dart
+++ lib/screendapt.dart
@@ -3,4 +3,5 @@ library screendapt;
 export 'src/s_container.dart';
 export 'src/s_icon.dart';
 export 'src/s_text.dart';
+export 'src/device_sizes.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
